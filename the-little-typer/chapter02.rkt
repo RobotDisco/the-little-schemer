312aa8c9f796758ed0b446786b4dc32602b63f66
Gaelan D'costa <gdcosta@gmail.com>
2024-04-07 20:43:07 -0400

    Finishing Little Typer, Chapter 2

--- the-little-typer/chapter02.rkt
+++ the-little-typer/chapter02.rkt
@@ -29,3 +29,45 @@
 ;;   (cons Nat Nat))
 (define Pear
   (Pair Nat Nat))
+
+;; Frame 93
+(claim Pear-maker U)
+(define Pear-maker
+  (-> Nat Nat
+      Pear))
+
+(claim elim-Pear
+  (-> Pear Pear-maker
+      Pear))
+(define elim-Pear
+  (lambda (pear maker)
+    (maker (car pear) (cdr pear))))
+
+;; wrote claim of elim-Pear without using Pear or Pear-maker
+;; (claim elim-Pear
+;;        (-> (Pair Nat Nat) (-> Nat Nat
+;;                             (Pair Nat Nat))
+;;            (Pair Nat Nat)))
+
+;; Frame 100
+(claim pearwise+
+       (-> Pear Pear
+           Pear))
+
+;; Doesn't work because + isn't defined
+;; this is incorrect but seems simpler?
+;;
+;; (define pearwise+
+;;   (lambda (pa pb)
+;;     (cons (+ (car pa) (car pb))
+;;           (+ (cdr pa) (cdr pb)))))
+
+;; Doesn't work because + isn't defined
+;; OK so what is the point of this frame?
+;;
+;; (define pearwise+
+;;   (lambda (pa pb)
+;;     (elim-Pear pa (lambda (paa pad)
+;;                     (elim-Pear pb (lambda (pba pbd)
+;;                                     (cons (+ paa pba)
+;;                                           (+ pad pbd))))))))
